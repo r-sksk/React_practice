e9bc2a2f0324046d3e8a98ec001c54358a4ab196
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-11-06 14:53:25 +0900

    finish

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container text-center">
+      <Clock />
+    </div>
+  )
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.now = new Date();
+
+    this.state = {
+      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+
+  refresh() {
+    this.now = new Date();
+
+    this.setState((state) => ({
+      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }));
+  }
+
+  render() {
+    return <p onClick={this.refresh}>{this.state.time}</p>
+  }
+}
+
+    export default App;
